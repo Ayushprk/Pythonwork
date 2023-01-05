0d3c3cae8e14dfabc72839cec78a9642178cfea8
Ayush Prakash <ayushprk.ap@gmail.com>
2023-01-05 12:10:05 -0500

    Practice 1

---
+++ Practice1b.py
@@ -0,0 +1,4 @@
+miles_traveled = float(input())
+gallons_of_gas_used = float(input())
+miles_per_gallon = miles_traveled / gallons_of_gas_used
+print(miles_per_gallon)
---
+++ Practice1c.py
@@ -0,0 +1,3 @@
+rent = float(input())
+cost_per_day = rent / 30
+print(cost_per_day)
