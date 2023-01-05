c36926f61377c0c0b39dad91a3675f21f06d50fd
Ayush Prakash <ayushprk.ap@gmail.com>
2023-01-05 12:27:28 -0500

    Second Practice

---
+++ Practice2a.py
@@ -0,0 +1,5 @@
+test1 = int(input("Enter score for test 1: "))
+test2 = int(input("Enter score for test 2: "))
+test3 = int(input("Enter score for test 3: "))
+average = (test1 + test2 + test3) / 3
+print("Average is ", average)
